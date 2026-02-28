92174167300527b84edeeeb66d3f432fc52e8178
claudio <611claudio@gmail.com>
2026-02-27 23:06:47 -0300

    implementar sistema de seletores dinâmicos e menu HTML/CSS: - Novos containers: .css-container-itens e [data-type=extendedBox]. - Classes específicas para o menu: .css-menu e .css-img-menu. JavaScript: - fechamento novo elemento por clicar body ou próprio botão - Lógica de ordenação baseada no atributo data-order. - Toggle de visibilidade - Efeito hover dinâmico via classe .selecting-itens.

--- main.js
+++ main.js
@@ -2,52 +2,99 @@
 function ClearAllExtendedBox(event){
   event.stopPropagation();
 
-  if(event.target.dataset.type!=="extendable"){
-    let i=0;
-
-    extendedBoxArr.forEach(box => {
-      const triggerElement = box.parentNode; // Find who the parent actually is
-      if (triggerElement) {
-        triggerElement.isExtended = false;
-        triggerElement.removeChild(box);
-      }
+  if(event.target.dataset.type!=="extendedBox"){
+    extendedBox.forEach(box => {
+      if (box.classList.hidden === undefined) box.classList.add("hidden");
     });
 
-    extendedBoxArr = [];
-    document.body.removeEventListener("click",this);
+    extendableElementList.forEach(triggerElement=>{
+      if(triggerElement) triggerElement.isExtended = false;
+    });
+
+    document.body.removeEventListener("click",ClearAllExtendedBox);
   }
 };
 
-const AppendMenu = (targetElement,trigger)=>{
-  targetElement.classList.add("css-menu");
-  trigger.isExtended = true;
+function RenderItens(parentElement,itemBox){
+  const btnSelector = parentElement.querySelector('[data-action="open-selector"]');
+  itemBox.addEventListener("mouseover",(event)=>{
+      if(event.target.dataset.order!==undefined)
+      event.target.classList.add("selecting-itens");
+    }
+  );
 
-  trigger.appendChild(targetElement);
-}
+  itemBox.addEventListener("mouseout",(event)=>{
+      if(event.target.dataset.order!==undefined)
+        event.target.classList.remove("selecting-itens");
+    }
+  );
+
+  const SelectItem = (event)=>{
+    const selected = event.target.closest('[data-order]');
+
+    //clean all elements
+    const oldSelected = btnSelector.firstElementChild;
+    if(oldSelected) selected.replaceWith(oldSelected);
+    btnSelector.appendChild(selected);
+
+    event.target.classList.remove("selecting-itens");
+    const elementList = Array.from(itemBox.children);
+    
+    //sort element by ID, using bubble sort
+    for(let i=0;i<elementList.length;i++){
+      for(let j=1;j<elementList.length-i;j++){
+        let before = elementList[j-1];
+        const beforeOrder = Number(before.dataset.order);
+        const currOrder = Number(elementList[j].dataset.order);
+
+        if(beforeOrder > currOrder) {
+          elementList[j-1] = elementList[j];
+          elementList[j] = before;
+        }else if(beforeOrder === currOrder) console.warn("data-order must be different!");
+      }
+    }
+    
+    elementList.forEach(el=>{
+      itemBox.appendChild(el);
+      }
+    );
+  };
+  
+  itemBox.addEventListener("click",SelectItem);
+};
 
 
-//variable
-let extendedBoxArr = [];
+//variable global
+
 
 //elements
 const extendableElementList = document.querySelectorAll('[data-type="extendable"]');
+const extendedBox = document.querySelectorAll('[data-type="extendedBox"]');
+const selector = document.querySelectorAll('.selector');
 
+extendedBox.forEach(box=>box.classList.add("hidden"));
 
 extendableElementList.forEach(trigger=>trigger.addEventListener("click",event=>{
   event.stopPropagation();
   const action = trigger.dataset.action;
-  let newBox;
 
-  if(!trigger.isExtended) newBox = document.createElement("div");
-  else return;
+  trigger.isExtended=!trigger.isExtended;
 
-  extendedBoxArr.push(newBox);
-
-  if(action === "open-menu") AppendMenu(newBox ,trigger);
-  //else if(action==="")
+  let targetElement;
   
+  if(action === "open-menu"){
+    targetElement = document.body.querySelector('[data-for="menu"]');
+  }
+  else if(action==="open-selector") {
+    const parentElement = trigger.parentNode;
+    targetElement = parentElement.querySelector('[data-type="extendedBox"]');
+    if(!trigger.initialized) RenderItens(parentElement,targetElement);
+  }
 
-  document.body.addEventListener("click",ClearAllExtendedBox);
 
-})
-)
+  if(trigger.isExtended){
+    targetElement.classList.remove("hidden");
+    document.body.addEventListener("click",ClearAllExtendedBox);
+  }else targetElement.classList.add("hidden");
+  }
+))
