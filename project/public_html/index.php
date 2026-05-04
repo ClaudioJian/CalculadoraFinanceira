1deb24a2bb7aede5f98f5b5edf6d24da79e850c4
gabriel-raciunas-menezes <gabriel.raciunas@gmail.com>
2026-05-04 20:24:57 -0300

    mudei o index

--- project/public_html/index.php
+++ project/public_html/index.php
@@ -1,30 +1,21 @@
 <?php 
 //security
+  session_start();
+  
+  //only allow user enter by index.php
+  define('is_allowed',true);
+  define('private_path', __DIR__ .'/../private');
 
-  //insert this code to other file if you want user to acess directly by typing folder name. like localhost/public/html/index.php
-
-  $init_file = __DIR__ .'/../private/' . 'init.php';
-
-  if(!is_file($init_file)){
+  if(!defined('private_path')){
     http_response_code(403);
     //die = exit
-    exit('init.php not finded:'.$init_file);
-  }
-
-  require_once $init_file;
-  //----------------------------------------------------------------------------------------------------------------------------------
-
-  if(!is_dir(PRIVATE_PATH)){
-    http_response_code(403);
-    exit('path not finded:'.PRIVATE_PATH);
+    die('path not finded');
   }
 
   //copy and paste for functions
   //current features(work in progress): create selector and calculator
-  require_once PRIVATE_PATH . '/processor/CustomElements/allElements.php';
+  require_once private_path . '\processor\createCustomElement.php';
 
-  //change to use processor\elements as anyname if you think it is too long to write elements
-  use processor\elements;
 
 
 
@@ -42,24 +33,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>*Nome da empresa*</title>
-    <link rel="stylesheet" href="esqueleto.css"/>
+    <link rel="stylesheet" href="finalesqueleto.css"/>
   </head>
   <body>
 
-      <main>
+    <main>
         <!--menu-->
         <div class="css-menu-container">
           <nav class="css-menu" data-type="extendedBox" data-for="menu" data-targetboxid="menu">
-            <a href="esqueleto.html">test</a>
+            <div class="menu-header"></div>
+            <div class="instrucao i8">texto aqui(login)</div>
+            <div class="menu-divider m1"></div>
+            <a href="esqueleto.html" class="test">test</a>
             <a href="https://github.com/ClaudioJian/CalculadoraFinanceira.git">teste-Github-Link</a>
+            <div class="instrucao i9">outras páginas(outras páginas)</div>
+            <div class="menu-divider m2"></div>
             <form>
               <input placeholder="test" name="test">
             </form>
-            
+            <div class="instrucao i10">texto aqui(histórico)</div>
+            <div class="menu-divider m3"></div>
             <div class="resizer" data-direction="e"></div>
           </nav>
           <!--button for open menu-->
-          <button style="height: fit-content;">
+          <button style="height: fit-content;" class="img-button">
             <img class="css-img-menu" data-action="open-menu" data-type="extendable" src="image/img_placeholder.jpg" data-openboxid="menu">
           </button>
           <!--button for open calculator-->
@@ -70,7 +67,7 @@
 
         <!--calculator-->
         <?php
-          elements\CUSTOM_ELEMENT::render('calculator');
+          create_custom_element('calculator',1);
         ?>
 
         
@@ -80,28 +77,38 @@
         <div class="instrucoes">
           <div class="instrucao i1">O que seria?</div>
           <div class="instrucao i2">Tipo?</div>
-          <div class="instrucao i3">Valores investidos<br> sequintes</div>
+          <div class="instrucao i3">Valor depositado</div>
           <div class="instrucao i4">Prazo</div>
-          <div class="instrucao i5">Tipo de gráfico</div>
+          <div class="instrucao i5">Gráfico:</div>
+          <div class="instrucao i6">Valores posteriores seguintes</div>
+          <div class="instrucao i7">Porcentual do valor</div>
         </div>
 
         <!--selector-->
         <?php
-          elements\CUSTOM_ELEMENT::render('selector',3);
+          create_custom_element('selector',3);
         ?>
 
         <div class="insert">
-          <div class="prazo"></div>
+          <input type="number" class="quantidade" placeholder="R$1000" required></input>
+          <input type="number" class="q_posterior" placeholder="R$1000" required></input>
+          <input type="number" class="q_porcentual" placeholder="101,5%" required ></input>
         </div>
 
         <!--selector-->
         <?php
           //loadQuant tell how many element you want to load from there, data for element must placed in order
-          elements\CUSTOM_ELEMENT::render('selector');
+          create_custom_element('selector',2);
         ?>
 
+      
+      <?php
+          create_custom_element('selector',4);
+        ?>
 
-
+        <?php
+          create_custom_element('selector',5);
+        ?>
 
         <!--buttons-->
         <div class="agrupment_button">
@@ -114,5 +121,4 @@
       <script src="main.js"></script>
   </body>
 </html>
-
 
